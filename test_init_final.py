2a454753e481bc95ec8d5564b4de55f4e061bd6e
daram22da <66212044+daram22da@users.noreply.github.com>
2020-06-02 22:51:30 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -643,10 +643,9 @@ async def task():
 
 #mp3 파일 생성함수(gTTS 이용, 남성목소리)
 async def MakeSound(saveSTR, filename):
-	
+	'''
 	tts = gTTS(saveSTR, lang = 'ko')
 	tts.save('./' + filename + '.mp3')
-	
 	'''
 	try:
 		encText = urllib.parse.quote(saveSTR)
